9ac7905c7e8c78b1d7acd8efdd82feb5a40f75c0
conejofoz <silviocoelho.udc@gmail.com>
2021-01-28 11:24:02 -0300

    login ok

--- devcondbk/app/Http/Controllers/AuthController.php
+++ devcondbk/app/Http/Controllers/AuthController.php
@@ -77,4 +77,47 @@ class AuthController extends Controller
 
         return $array;
     }
+
+
+    public function login(Request $request){
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(),[
+            'cpf'=>'required|digits:11',
+            'password'=>'required'
+        ]);
+
+        if(!$validator->fails()){
+            $cpf = $request->input('cpf');
+            $password = $request->input('password');
+
+            $token = Auth::attempt([
+            //$token = auth()->attempt([
+                'cpf'=> $cpf,
+                'password'=>$password
+            ]);  
+
+            if(!$token){
+                $array['error'] = 'CPF e/ou senha estão errados!';
+                return $array;
+            }
+
+            $array['token'] = $token;
+
+            $user = auth()->user();
+            $array['user'] = $user;
+
+            $properties = Unit::select(['id', 'name'])
+            ->where('id_owner', $user['id'])
+            ->get();
+
+            $array['user']['properties'] = $properties;
+
+        } else {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+
+        return $array;
+    }
 }
